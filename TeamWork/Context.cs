889a86a0afa61cfd3204374c6723753e33da9a64
pr0alex <prooalex@gmail.com>
2016-01-13 14:37:57 +0300

    Add Context

---
+++ TeamWork/Context.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamWork
+{
+    class Context : DbContext
+    {
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<University> Universities { get; set; }
+    }
+}
